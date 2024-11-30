1bd5338a4632ed9ffb14c84e2f29da1155f098c2
Ulul Azmi <85116521+ululazmi18@users.noreply.github.com>
2024-11-30 15:51:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import os
+
+# Daftar jalur folder yang ingin dibuat
+folders = [
+    os.path.join(os.path.dirname(__file__), 'sessions', 'gramjs_string'),
+    os.path.join(os.path.dirname(__file__), 'sessions', 'lupa'),
+    os.path.join(os.path.dirname(__file__), 'sessions', 'pyrogram'),
+    os.path.join(os.path.dirname(__file__), 'sessions', 'telethon'),
+    os.path.join(os.path.dirname(__file__), 'selesai', 'gramjs_string'),
+    os.path.join(os.path.dirname(__file__), 'selesai', 'pyrogram'),
+    os.path.join(os.path.dirname(__file__), 'selesai', 'telethon'),
+    os.path.join(os.path.dirname(__file__), 'core', 'lab', '1'),
+    os.path.join(os.path.dirname(__file__), 'core', 'lab', '2'),
+    os.path.join(os.path.dirname(__file__), 'core', 'lab', '3'),
+    os.path.join(os.path.dirname(__file__), 'core', 'lab', '4'),
+]
+
+# Membuat folder jika belum ada
+for folder in folders:
+    os.makedirs(folder, exist_ok=True)
+
 def main():
     from helpers import menu
     # Memanggil fungsi menu utama
